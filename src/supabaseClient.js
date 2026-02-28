044e83c58c2ac2dfef2943a9d5b260e3fc5e3623
thotarohit05 <thota.rohit0505@gmail.com>
2026-02-28 19:34:45 +0530

    Update supabase client configuration

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,8 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
